ddf5fbe0ec52c29ebc4660554070e179ea6a0685
Radouane Chihaby <rchihaby@ebpt.transdev>
2023-06-13 10:01:15 -0700

    adds google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Script from 'next/script'
 import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2CN95RFCCM" />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-2CN95RFCCM');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
