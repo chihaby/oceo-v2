d5e96eea26f33fcc668454836f240bacfde812ec
Radouane Chihaby <chihaby@gmail.com>
2025-03-07 11:24:37 -0800

    deployement 4

--- pages/api/saveToGoogleSheets.js
+++ pages/api/saveToGoogleSheets.js
@@ -1,5 +1,5 @@
 import { google } from "googleapis";
-import credentials from "../../credentials/chiCredentials.json";
+import credentials from "../../credentials/oceoCredentials.json";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
