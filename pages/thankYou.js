a4ea3653a42a022fb42f76051bdf59593ee6bc07
Radouane Chihaby <chihaby@gmail.com>
2025-04-16 08:54:48 -0700

    edits a thank you page

--- pages/thankYou.js
+++ pages/thankYou.js
@@ -1,11 +1,31 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { Container, Row, Col } from "react-bootstrap";
 
-const thankYou = () => {
+function ThankYou() {
   return (
-    <div>
-      <Link href={'/'}>Home</Link>
-    </div>
-  )
+    <>
+      <Container>
+        <br />
+        <br />
+        <Row className="justify-content-md-center">
+          <Col xs lg="2"></Col>
+          <Col md="auto">
+            <h2>Thank You</h2>
+            <br />
+            <p>
+              Thank you for contacting me. I will review your message and get
+              back to you as soon as possible.
+            </p>
+            <p>
+              <em>Oceo</em>
+            </p>
+            <Link href="/">Return to Home page</Link>
+          </Col>
+          <Col xs lg="2"></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default thankYou
+export default ThankYou;
