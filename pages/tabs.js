e529cbd58c8cf234873b247b3690cf54d756c162
Rad <chihaby@gmail.com>
2025-06-12 08:55:59 -0700

    edits tab page

--- pages/tabs.js
+++ pages/tabs.js
@@ -1,11 +1,12 @@
-import Tabs from "@/components/Tabs";
-import Footer from "../components/Footer";
-import styles from "../styles/TabsPage.module.css";
+import Tabs from '@/components/Tabs';
+import Footer from '../components/Footer';
+import styles from '../styles/TabsPage.module.css';
 
 const tabs = () => {
   return (
     <div>
       <Tabs />
+      <br />
       <div className={styles.footer}>
         <Footer />
       </div>
