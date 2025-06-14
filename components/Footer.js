5eca99730566ea5bdabaa4c2dbcdf0e355a76e28
Rad <chihaby@gmail.com>
2025-06-13 22:48:21 -0700

    changes menu bar color

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Link from 'next/link'
-import Subscribe from './Subscribe'
-import styles from '../styles/Footer.module.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Link from 'next/link';
+import Subscribe from './Subscribe';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   return (
@@ -11,73 +11,102 @@ const Footer = () => {
       <Container>
         <Row>
           <Col xs={12} md={3}>
-            <Link href='/' passHref style={{ color: 'inherit', textDecoration: 'inherit'}} >
-              <span style={{fontSize: '36px', fontWeight: '600'}}>Oceo</span>
+            <Link
+              href='/'
+              passHref
+              style={{ color: 'inherit', textDecoration: 'inherit' }}
+            >
+              <span style={{ fontSize: '36px', fontWeight: '600' }}>Oceo</span>
             </Link>
             <Subscribe />
           </Col>
           <Col xs={6} md={3}>
             <br />
             <br />
-              <span>
-                <i className="fa fa-bars"> SITE MAP</i>
-              </span>{' '}
-              <br />
-              <Col >
-                <ul >
-                  <li className={styles.li}>
-                    <Link href='/' passHref><span className={styles.site_map}> Home</span></Link>
-                  </li>
-                  <li className={styles.li}>
-                    <Link href='/about' passHref><span className={styles.site_map}>About</span></Link>
-                  </li>    
-                  <li className={styles.li}>
-                    <Link href='/music' passHref><span className={styles.site_map}>Music</span></Link>
-                  </li>         
-                </ul>
-              </Col>
+            <span>
+              <i className='fa fa-bars'> SITE MAP</i>
+            </span>{' '}
+            <br />
+            <Col>
+              <ul>
+                <li className={styles.li}>
+                  <Link href='/' passHref>
+                    <span className={styles.site_map}> Home</span>
+                  </Link>
+                </li>
+                <li className={styles.li}>
+                  <Link href='/about' passHref>
+                    <span className={styles.site_map}>About</span>
+                  </Link>
+                </li>
+                <li className={styles.li}>
+                  <Link href='/music' passHref>
+                    <span className={styles.site_map}>Music</span>
+                  </Link>
+                </li>
+              </ul>
+            </Col>
           </Col>
           <Col xs={6} md={3}>
             <br />
             <br />
-            <ul >         
+            <ul>
               <li className={styles.li}>
-                <Link href='/services' passHref><span className={styles.site_map}>Services</span></Link>
-              </li>   
+                <Link href='/services' passHref>
+                  <span className={styles.site_map}>Services</span>
+                </Link>
+              </li>
               {/* <li className={styles.li}>
                 <Link href='/blog' passHref><span className={styles.site_map}>Blog</span></Link>
               </li>            */}
               <li className={styles.li}>
-                <Link href='/contact' passHref><span className={styles.site_map}>Contact</span></Link>
+                <Link href='/contact' passHref>
+                  <span className={styles.site_map}>Contact</span>
+                </Link>
               </li>
             </ul>
           </Col>
           <Col xs={12} md={3}>
-          <div className={styles.contact_email}>
-          <br />
-          <br />
-          <p>
-            <span>
-              <i className='fa fa-envelope'> EMAIL</i> 
-            </span>{' '}
-            <br /><br />
-            {'oceomusic@gmail.com'}
-          </p>
-        </div>
-          <div className={styles.copyright}>
-            {/* <p> Copyright &copy; 2023 - Oceo Music</p> */}
- 
+            <div className={styles.contact_email}>
+              <br />
+              <br />
+              <p>
+                <span>
+                  <i className='fa fa-envelope'> EMAIL</i>
+                </span>{' '}
+                <br />
+                <br />
+                {'oceomusic@gmail.com'}
+              </p>
+            </div>
+            <div className={styles.copyright}>
+              {/* <p> Copyright &copy; 2023 - Oceo Music</p> */}
               <span>
-                <i className='fa fa-envelope'>Website</i> 
+                <i className='fa fa-envelope'>Website</i>
               </span>{' '}
-
-            <span>Created by </span><Link style={{ color: 'inherit', textDecoration: 'inherit'}} href={"https://www.websitetoro.com/"} rel="noopener noreferrer" target="_blank"><span style={{textDecoration: 'none', fontSize: '20px', fontWeight: '700'}}><em>Website Toro</em></span></Link>
-          </div>
+              <span>Created by </span>
+              <Link
+                style={{ color: 'inherit', textDecoration: 'inherit' }}
+                href={'https://www.websitetoro.com/'}
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                <span
+                  style={{
+                    textDecoration: 'none',
+                    fontSize: '20px',
+                    fontWeight: '700',
+                  }}
+                >
+                  <em>Website Remedy</em>
+                </span>
+              </Link>
+            </div>
           </Col>
         </Row>
-      </Container>    
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Tabs.js
+++ components/Tabs.js
@@ -1,51 +1,51 @@
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
 // import Row from "react-bootstrap/Row";
 // import Col from "react-bootstrap/Col";
 // import Card from "react-bootstrap/Card";
 // import Link from "next/link";
 // import styles from "../styles/Tabs.module.css";
-import { useState } from "react";
+import { useState } from 'react';
 
 const Tabs = () => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   const handleSignup = async (event) => {
     event.preventDefault();
     setLoading(true);
 
     // Save to Google Sheets
-    await fetch("/api/saveToGoogleSheets", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    await fetch('/api/saveToGoogleSheets', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email }),
     });
 
     // Save to Mailchimp
-    await fetch("/api/saveToMailchimp", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    await fetch('/api/saveToMailchimp', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email }),
     });
 
     try {
-      const response = await fetch("/api/sendEmail", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
       });
 
       const data = await response.json();
       if (data.success) {
         setMessage(
-          "Thank you for signing up! Your guitar tabs are on their way to your inbox.\n Be sure to check your spam/junk folder for the download link. Happy playing!"
+          'Thank you for signing up! Your guitar tabs are on their way to your inbox.\n Be sure to check your spam/junk folder for the download link. Happy playing!'
         );
       } else {
-        alert("Error sending email: " + data.error);
+        alert('Error sending email: ' + data.error);
       }
     } catch (error) {
-      console.error("Signup error:", error);
+      console.error('Signup error:', error);
     } finally {
       setLoading(false); // Hide spinner when done
     }
@@ -57,28 +57,28 @@ const Tabs = () => {
       <h1>Tabs</h1>
       <br />
       <div>
-        {"Get Evora guitar tabs"} 🎸 <br />
+        {'Get Evora guitar tabs'} 🎸 <br />
         {
-          "Enter your email below, and you will receive a download link to your inbox"
+          'Enter your email below, and you will receive a download link to your inbox'
         }
         <br />
-        {"Plus, you will get updates on new music, lessons, and much more."}
+        {'Plus, you will get updates on new music, lessons, and much more.'}
       </div>
       <br />
       <form onSubmit={handleSignup}>
         <input
-          type="email"
-          placeholder="Enter your email"
+          type='email'
+          placeholder='Enter your email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-        <button type="submit" disabled={loading}>
-          {loading ? "Subscribing..." : "Subscribe"}
+        <button type='submit' disabled={loading}>
+          {loading ? 'Subscribing...' : 'Get Tabs'}
         </button>
       </form>
       <div>
-        {loading && <div className="spinner"></div>}
+        {loading && <div className='spinner'></div>}
 
         {message && <p>{message}</p>}
 
