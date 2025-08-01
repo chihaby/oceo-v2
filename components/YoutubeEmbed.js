a2d2cce9194304a4002791fc1ad14c90a152f4ef
Radouane Chihaby <chihaby@gmail.com>
2025-08-01 14:44:10 -0700

    updates youtub[Ce video

--- components/YoutubeEmbed.js
+++ components/YoutubeEmbed.js
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
-import styles from '../styles/YoutubeEmbed.module.css'
+import styles from "../styles/YoutubeEmbed.module.css";
 
 const YoutubeEmbed = () => (
-<div className={styles.video_responsive}>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/leocfFO_N0o" title="Oceo - Andalucia" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-</div>
+  <div className={styles.video_responsive}>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/gprEBs0Stf4?si=xhRtoKTIjAPGO6WL"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  </div>
 );
 
 YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
+  embedId: PropTypes.string.isRequired,
 };
 
 export default YoutubeEmbed;
